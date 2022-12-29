afe791e70a73a04513a04052f6ffd27cd6a9b56c
Anas jaidi <anas.jaidi@icloud.com>
2022-12-29 20:58:24 +0100

    finish vars and types

--- book/book/src/guess_game.rs
+++ book/book/src/guess_game.rs
@@ -1,33 +1,31 @@
-use std::io;
+use colored::*;
 use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
-use colored::*;
-
+use std::io;
 
 fn main() {
-  println!("give a number:");
-  let mut thread = thread_rng();
-  let secret_key = thread.gen_range(1..=100);
-  
-  loop {
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed");
-  let guess : u32 = match guess.trim().parse() {
-    Ok(i)=> i,
-    Err(_) => continue,
-  };
-  match guess.cmp(&secret_key) {
-    Ordering::Greater => {
-      println!("{}","to big".red());
-    },
-    Ordering::Less => {
-      println!("{}","to low".red());
-    },
-    Ordering::Equal => {
-      println!("{}", "you win".green());
-      break;
+    println!("give a number:");
+    let mut thread = thread_rng();
+    let secret_key = thread.gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(i) => i,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_key) {
+            Ordering::Greater => {
+                println!("{}", "to big".red());
+            }
+            Ordering::Less => {
+                println!("{}", "to low".red());
+            }
+            Ordering::Equal => {
+                println!("{}", "you win".green());
+                break;
+            }
+        };
     }
-  };
-  }
-  
 }
---
+++ book/book/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+  let mut i: u8 = 255;
+  i +=1;
+}
