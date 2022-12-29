a4b40c6b0bd1deaf33d52f7e1c7d90d6e05dbd64
Anas jaidi <anas.jaidi@icloud.com>
2022-12-29 22:40:32 +0100

    slices

---
+++ book/book/src/slice.rs
@@ -0,0 +1,31 @@
+fn main() {
+  let hw = String::from("Hello, World!");
+  let hws = &hw[..];
+  let h = "Hello world"; // both hello and h are string slice &str because they point directly into memory
+  let arr = [1,1,1];
+  let arrs = &a[..2];
+  let hw = "Hello, World!";
+  let hello = first_world(hw);
+
+}
+
+fn first_world(s: &str) -> &str {
+  let bytes: &[u8] = s.as_bytes();
+  for (i: usize, &item: u8) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return i;
+    }
+  }
+  &s[..i] // return a slice a pointer to a portion of actual data
+}
+
+
+// fn first_world(s: &String) -> usize {
+//   let bytes: &[u8] = s.as_bytes();
+//   for (i: usize, &item: u8) in bytes.iter().enumerate() {
+//     if item == b' ' {
+//       return i;
+//     }
+//   }
+// s.1en()
+// }
