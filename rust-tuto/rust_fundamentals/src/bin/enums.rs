1d128c7b2ae7dc55812d64f651fa589168a49ffb
Anas jaidi <anas.jaidi@icloud.com>
2023-09-15 07:52:02 +0100

    chore: add enums

---
+++ rust-tuto/rust_fundamentals/src/bin/enums.rs
@@ -0,0 +1,22 @@
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right // comma in last variant is optional
+}
+
+fn dir(d: Direction) -> i8 {
+    return match d {
+        Direction::Down => 1,
+        Direction::Left => 2,
+        Direction::Right => 3,
+        Direction::Up => 4,
+    }
+}
+
+fn main() {
+    let a = Direction::Down;
+
+    println!("{:?}", dir(a));
+}
--- rust-tuto/rust_fundamentals/src/bin/match.rs
+++ rust-tuto/rust_fundamentals/src/bin/match.rs
@@ -12,4 +12,20 @@ fn main() {
         },
         _ => println!("yes"), // default
     }
+    let age = 25;
+
+    match age {
+        n if n > 20 => {
+            println!("Age is greater than 20: {}", n);
+            // Additional code for age greater than 20
+        }
+        n if n < 20 => {
+            println!("Age is less than 20: {}", n);
+            // Additional code for age less than 20
+        }
+        _ => {
+            println!("Age is equal to 20");
+            // Additional code for age equal to 20 or other cases
+        }
+    }
 }
