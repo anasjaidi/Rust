69b90c3b2f441ecd6d885fc46e58ea77fa4bc829
Anas jaidi <anas.jaidi@icloud.com>
2023-09-17 10:27:09 +0100

    chore: add tuples

---
+++ rust-tuto/rust_fundamentals/src/bin/tuples.rs
@@ -0,0 +1,22 @@
+enum EmployeeAccess {
+    Full,
+    Hybrid
+}
+
+fn one_two_three() -> (i8, i8, i8){
+    return (1, 2, 3);
+}
+
+fn main() {
+    let nums = one_two_three();
+    let  (x, y, z)= one_two_three();
+    println!("{:?} == {:?}", nums.0, x);
+    println!("{:?} == {:?}", nums.1, y);
+    println!("{:?} == {:?}", nums.2, z);
+
+    let employee_1 = ("anas jaidi", EmployeeAccess::Hybrid);
+
+    let (employee_name, employee_access) = employee_1;
+
+    println!("{:?}", employee_1.0);
+}
