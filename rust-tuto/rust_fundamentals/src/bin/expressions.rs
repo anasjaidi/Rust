5e3135fb4662729967810b3ca0da2d81e2a3118c
Anas jaidi <anas.jaidi@icloud.com>
2023-09-18 06:26:46 +0100

    chore: basic demo on expressions

--- rust-tuto/rust_fundamentals/src/bin/expressions.rs
+++ rust-tuto/rust_fundamentals/src/bin/expressions.rs
@@ -1,17 +1,32 @@
-fn main() {
-    let a = if 1 == 1 {
-        12
-    } else {
-        13
+enum AccessType {
+    Admin,
+    Basic,
+    Guest,
+    Manager,
+}
+
+fn check_access(access_type: AccessType) -> bool{
+    let user_have_access = match access_type {
+        AccessType::Admin => true,
+        AccessType::Manager => true,
+        _ => false,
     };
+    user_have_access
+}
+
+fn main() {
+    let a = if 1 == 1 { 12 } else { 13 };
 
     println!("{:?}", a);
 
     let b = match 1 {
         1 => 10,
         2 => 20,
-        _ => 30
+        _ => 30,
     };
 
     println!("{:?}", b);
+
+    let user_access_type = AccessType::Admin;
+    println!("{:?}", check_access(user_access_type));
 }
