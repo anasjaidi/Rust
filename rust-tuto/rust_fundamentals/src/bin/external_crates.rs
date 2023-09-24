327094d12322cb44d0daab7e6711a9306dcf71a9
Anas jaidi <anas.jaidi@icloud.com>
2023-09-24 18:01:45 +0100

    chore : add external crate

--- rust-tuto/rust_fundamentals/src/bin/external_crates.rs
+++ rust-tuto/rust_fundamentals/src/bin/external_crates.rs
@@ -1,5 +1,6 @@
-use humantime::*;
-
+use humantime::{format_duration};
+use std::time::Duration;
 fn main() {
-    
+    let d = Duration::from_secs(12000);
+    println!("{}", format_duration(d))
 }
