cec430eb90328b071adce7ce647f15721faefb68
Anas jaidi <anas.jaidi@icloud.com>
2023-09-25 12:12:06 +0100

    finish: aliases for crates using

--- rust-tuto/external_module/src/main.rs
+++ rust-tuto/external_module/src/main.rs
@@ -1,9 +1,9 @@
-use my_lib::parser;
+use my_lib::parser as prs;
 
 mod exec;
 
 fn main() {
     println!("Hello, world!");
-    parser::p();
+    prs::p(); // prs is alias for parser
     exec::anasjaidi();
 }
