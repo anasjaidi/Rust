0356a38f5ba23ec78484c339229665643f6be211
Anas jaidi <anas.jaidi@icloud.com>
2023-09-28 09:50:28 +0100

    chore: add simple main to test program

--- snaphood/src/main.rs
+++ snaphood/src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+use snaphood::data::*;
 fn main() {
-    println!("Hello, world!");
+    // Retrieve the provided file paths passed as arguments
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let allowed_flags = get_allowed_flags();
+
+    let map = snaphood::parser::parse_input(&args, &allowed_flags);
+
+    let map = match map {
+        Err(err) => {
+            err.print();
+            std::process::exit(1);
+        },
+        Ok(map) => map
+    };
+    let _map = dbg!(map);
 }
