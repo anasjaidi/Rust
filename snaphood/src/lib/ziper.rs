48226253f0589b9482fa6b11e521e3c1252f3f4d
Anas jaidi <anas.jaidi@icloud.com>
2023-11-26 14:17:27 +0100

    snaphood

--- snaphood/src/lib/ziper.rs
+++ snaphood/src/lib/ziper.rs
@@ -1,1 +1,18 @@
-pub fn zip(files: &Vec<String>, path: Option<&String>, filename: &String) {}
+use std::{fs::{self, File}, path::{Path, PathBuf}};
+
+pub fn zip(files: &Vec<String>, path: Option<&String>, filename: &String) {
+
+    let f = PathBuf::from("~").join(format!("{}.zip", filename)).to_str().unwrap();
+    let file_path = match path {
+        None => {
+        
+        }
+        Some(p) => 
+    };
+    match fs::create_dir_all(file_path) {
+        Err(_err) => {}
+        _ => {}
+    };
+    let file = File::create(file_path);
+
+}
