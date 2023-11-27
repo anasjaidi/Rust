59c5364f3a3394d6d6ac50fa444a8d91741d1dce
Anas jaidi <anas.jaidi@icloud.com>
2023-11-27 23:46:07 +0100

    add: memoset in c and rust style

--- libft/src/lib.rs
+++ libft/src/lib.rs
@@ -1,16 +1,32 @@
 // void *memset(void *b, int c, size_t len);
 
-fn meme_set(ptr: *mut u8, c: u8, len: usize) -> *mut u8 {
+use std::ffi::c_void;
+use libc::{c_char, c_int, size_t};
 
-    if ptr.is_null() {return ptr}
-    let mut bytes = unsafe {
-        std::slice::from_raw_parts_mut( ptr, len)
-    };
-    for b in  bytes.iter_mut() {
-        *b = c;
+
+fn meme_set_rust(ptr: *mut u8, c: u8, len: usize) -> *mut u8 {
+    if ptr.is_null() { return ptr }
+
+    for i in 0..len {
+        unsafe {*(ptr.add(i)) = c as u8;}
+    }
+
+    ptr
+}
+
+extern "C" fn mem_set_c(ptr: *mut c_void, c: c_int, len: size_t) -> *mut c_void {
+    unsafe {
+        if ptr.is_null() {
+            return ptr
+        }
+        for i in 0..len {
+            *((ptr as *mut c_char).add(i))  = c as c_char;
+        }
     }
     ptr
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -18,16 +34,12 @@ mod tests {
 
     #[test]
     fn test_meme_set() {
-        let buffer = Box::new(12);
-        let ptr: *mut u8 = buffer.as_mut_ptr();
-
-        meme_set(ptr, 65, 4);
-
-        for i in 0..4 {
-            let byte = unsafe { *(ptr.add(i)) };
-            let ch = char::from(byte);
-
-            println!("Byte as char: {:?}", ch);
+        let mut buffer = [65u8; 12];
+        unsafe {
+            meme_set_rust(buffer.as_mut_ptr(), 0, 12);
+        }
+        for i in 0..12 {
+            println!("{}", buffer[i]);
         }
     }
 }
