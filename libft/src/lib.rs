ea38b9231bcb6a6649865b7278b13dd1d26a3d3a
Anas jaidi <anas.jaidi@icloud.com>
2023-11-28 01:54:30 +0100

    add: memcpy in c and rust style

--- libft/src/lib.rs
+++ libft/src/lib.rs
@@ -1,6 +1,7 @@
 // void *memset(void *b, int c, size_t len);
 
 use std::ffi::c_void;
+use std::ptr::null;
 use libc::{c_char, c_int, size_t};
 
 
@@ -50,6 +51,29 @@ extern "C" fn bzero_c(ptr: *mut c_void, len: size_t) -> *mut c_void {
     ptr
 }
 
+fn memcpy_rust(dst: *mut u8,src: *mut u8, len: usize) -> *mut u8 {
+    if dst.is_null() || src.is_null() {
+        return dst
+    }
+    for i in 0..len {
+        unsafe {
+            *(dst.add(i)) = *(src.add(i));
+        }
+    }
+    dst
+}
+
+extern "C" fn memcpy_c(dst: *mut c_void,src: *mut c_void, len: size_t) -> *mut c_void {
+    if dst.is_null() || src.is_null() {
+        return dst
+    }
+    for i in 0..len {
+        unsafe {
+            *((dst as *mut c_char).add(i)) = *((src as *mut c_char).add(i));
+        }
+    }
+    dst
+}
 
 
 #[cfg(test)]
