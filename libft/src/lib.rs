941cea346ad9fbc429ef8127e6d9926901c4de65
Anas jaidi <anas.jaidi@icloud.com>
2023-11-28 02:03:55 +0100

    add: memccpy in c and rust style

--- libft/src/lib.rs
+++ libft/src/lib.rs
@@ -1,8 +1,7 @@
 // void *memset(void *b, int c, size_t len);
 
 use std::ffi::c_void;
-use std::ptr::null;
-use libc::{c_char, c_int, size_t};
+use libc::{c_char, c_int, c_uchar, c_uint, size_t};
 
 
 fn meme_set_rust(ptr: *mut u8, c: u8, len: usize) -> *mut u8 {
@@ -73,6 +72,36 @@ extern "C" fn memcpy_c(dst: *mut c_void,src: *mut c_void, len: size_t) -> *mut c
         }
     }
     dst
+}
+
+fn memccpy_rust(dst: *mut u8,src: *mut u8,c: isize, len: usize) -> *mut u8 {
+    if dst.is_null() || src.is_null() {
+        return std::ptr::null_mut()
+    }
+    for i in 0..len {
+        unsafe {
+            if *(dst.add(i)) == (c as u8) {
+                return dst.add(i + 1);
+            }
+            *(dst.add(i)) = *(src.add(i));
+        }
+    }
+    std::ptr::null_mut()
+}
+
+extern "C" fn memccpy_c(dst: *mut c_void,src: *mut c_void,c: c_int, len: size_t) -> *mut c_void {
+    if dst.is_null() || src.is_null() {
+        return std::ptr::null_mut()
+    }
+    for i in 0..len {
+        unsafe {
+            if *(dst.add(i) as *mut c_uchar)  == (c as c_uchar) {
+                return dst.add(i + 1);
+            }
+            *((dst as *mut c_char).add(i)) = *((src as *mut c_char).add(i));
+        }
+    }
+    return std::ptr::null_mut()
 }
 
 
