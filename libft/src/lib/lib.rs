4a35731317e2062c6cfbbc1b8a01b78acf6dadfa
Anas jaidi <anas.jaidi@icloud.com>
2023-11-28 04:16:36 +0100

    enhance: libft by creating lib that exports sub modules resources

--- libft/src/lib/lib.rs
+++ libft/src/lib/lib.rs
@@ -1,24 +1,2 @@
-// void *memset(void *b, int c, size_t len);
-
-mod rust;
-mod c;
-
-use std::ffi::c_void;
-use libc::{c_char, c_int, c_uchar, c_uint, size_t};
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_meme_set() {
-        let mut buffer = [65u8; 12];
-        // unsafe {
-        //     bzero_rust(buffer.as_mut_ptr(), 12);
-        // }
-        // for i in 0..12 {
-        //     println!("{}", buffer[i]);
-        // }
-    }
-}
+pub mod rust;
+pub mod c;
