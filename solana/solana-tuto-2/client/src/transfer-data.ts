a59ae46796ac48c3fe68032bc1b37f2308fddb46
Anas jaidi <anas.jaidi@icloud.com>
2024-02-06 16:10:32 +0100

    add manual is

--- solana/solana-tuto-2/client/src/transfer-data.ts
+++ solana/solana-tuto-2/client/src/transfer-data.ts
@@ -2,8 +2,10 @@ import {
   Keypair,
   LAMPORTS_PER_SOL,
   PublicKey,
+  SystemInstruction,
   SystemProgram,
   Transaction,
+  TransactionInstruction,
   sendAndConfirmTransaction,
 } from '@solana/web3.js';
 import { connect } from './utils/connect';
@@ -13,19 +15,38 @@ import { getBalanceInSol } from './balance';
 const transfer = async (from: Keypair, to: PublicKey, amount: number) => {
   const connection = await connect();
   const tx = new Transaction();
-  const is = SystemProgram.transfer({
+  const autoIs = SystemProgram.transfer({
     fromPubkey: from.publicKey,
     toPubkey: to,
     lamports: amount,
   });
-  tx.add(is);
+  const isData = Buffer.alloc(12); // 4 for type + 8 for Lmaports
+  isData.writeUint32LE(2, 0);
+  isData.writeBigUint64LE(BigInt(amount), 4);
+  const manualIs = new TransactionInstruction({
+    keys: [
+      {
+        isSigner: true,
+        isWritable: true,
+        pubkey: from.publicKey,
+      },
+      {
+        isSigner: false,
+        isWritable: true,
+        pubkey: to,
+      },
+    ],
+    programId: SystemProgram.programId,
+    data: isData,
+  });
+  tx.add(manualIs);
   const txh = await sendAndConfirmTransaction(connection, tx, [from]);
   return txh;
 };
 
 transfer(kp, rkp.publicKey, 0.5 * LAMPORTS_PER_SOL).then(async (txh) => {
-    await transfer(rkp, kp.publicKey, 0.4 * LAMPORTS_PER_SOL)
-    // const b = getBalanceInSol(rkp.publicKey);
-    // console.log(b)
+  await transfer(rkp, kp.publicKey, 0.49 * LAMPORTS_PER_SOL);
+  const b = await getBalanceInSol(rkp.publicKey);
+  console.log(b);
   console.log(txh);
 });
